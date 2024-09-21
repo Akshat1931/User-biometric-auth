c4b376b98d3b42998c6d3b821c1e2f58d31cc8a4
Abhay Kumar <abhay292005@gmail.com>
2024-09-21 11:12:32 +0530

    Initial commit: home page uploaded

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      Created by P203
+      <a
+        href="https://gi"
+        target="_blank"
+        alt="Abhay's github account"
+        className="hover:underline"
+      >
+       AbhayAkshitVishnu
+      </a>
+    </h6>
+  );
+}
+
+export default Footer;
